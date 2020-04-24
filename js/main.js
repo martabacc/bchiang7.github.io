dc6b7ce1f2fad526301c391c31b4fd301b6e6813
Ronauli Silva <ronauli.sidabukke@btpn.com>
2020-04-24 20:24:27 +0700

    add dynamic greeting

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
+const options = {
+  timeZone: 'America/New_York',
+  hour12: false,
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+};
+
 $(function() {
   const d = new Date();
   const hours = d.getHours();
+
+  const data = [
+    [0, 4, 'Tidur atuh udah jam segini'],
+    [5, 11, 'Pagi pak bos!'], //Store messages in an array
+    [12, 17, 'Sore sayang'],
+    [18, 21, 'Malem, kau lagi ngapainnn'],
+    [21, 24, 'Nitenite sayang'],
+  ];
+
+  for (let i = 0; i < data.length; i++) {
+    if (hours >= data[i][0] && hours <= data[i][1]) {
+      greeting = data[i][2];
+    }
+  }
+
   const night = hours >= 19 || hours <= 7; // between 7pm and 7am
   const body = document.querySelector('body');
   const toggle = document.getElementById('toggle');
   const input = document.getElementById('switch');
+  const greetText = document.getElementById('greet');
+  const timeText = document.getElementById('time__slot');
 
   if (night) {
     input.checked = true;
     body.classList.add('night');
   }
+
+  greetText.innerHTML = greeting;
+  timeText.innerHTML = d.toLocaleString('en-US', options);
 
   toggle.addEventListener('click', function() {
     const isChecked = input.checked;
